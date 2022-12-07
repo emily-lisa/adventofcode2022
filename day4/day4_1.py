5e5a376773dc42c55d8f43b22ef6e3d59155085b
Emily Lisa Kock <emily.kock1998@gmail.com>
2022-12-08 00:05:31 +0100

    day4

--- day4/day4_1.py
+++ day4/day4_1.py
@@ -7,9 +7,7 @@ with open('input.txt', 'r') as f:
     for line in lines:
         if int(line[0][0]) >= int(line[1][0]) and int(line[0][1]) <= int(line[1][1]):
             total += 1
-            print(int(line[0][0]),  int(line[0][1]), '<=',   int(line[1][0]), int(line[1][1]))
         elif int(line[1][0]) >= int(line[0][0]) and int(line[1][1]) <= int(line[0][1]):
             total += 1
-            print(int(line[0][0]),  int(line[0][1]), '>=', int(line[1][0]), int(line[1][1]))
 
     print(total)
